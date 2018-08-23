5c944a5e8be7e7abda2a5998eed5ba926854c98b
Rodrigo Montes <rjmontes@gmail.com>
2018-08-23 14:03:35 -0300

    Seccion 4 Clase 134: - Inicio de creación de metodo para apuntar al "crosshair" del jugador - Se crean variables y metodo que identifica el punto de la mira de acuerdo al tamaño de la pantalla - TODO: "de-proyectar desde el punto del crosshair hasta el mundo" para obtener la orientacion de la mira - TODO: apuntar hacia donde esta el punto de la mira

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -20,7 +20,47 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	AimAtCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimAtCrosshair()
+{
+	if (!GetControlledTank()) {	return; }
+
+	FVector HitLocation;
+
+	if (GetSightRayHitLocation(OUT HitLocation))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Aiming at %s"), *HitLocation.ToString());
+		//TODO implement turret movement for aim
+	}
+
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(OUT FVector& OutHitLocation) const
+{
+	OutHitLocation = FVector(1.0);  //TODO remove!
+
+	//find crosshair position
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(OUT ViewportSizeX, OUT ViewportSizeY);
+
+	FVector2D ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+
+	//"de-project" the screen position into world position
+
+	//cast a ray from crosshair position in de-projection direction
+		//if hit something
+			//return true and HitLocation equal to raycast hit location
+		//else return false
+	return true;
 }
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#define OUT
+
 #include "Tank.h"
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
@@ -16,8 +18,20 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 
+private:
+	ATank* GetControlledTank() const;
+	void AimAtCrosshair();
+	bool GetSightRayHitLocation(OUT FVector& OutHitLocation) const;
+
+	UPROPERTY(EditAnywhere, Meta = (CrosshairX))
+		float CrossHairXLocation = 0.5f;
+
+	UPROPERTY(EditAnywhere, Meta = (CrosshairY))
+		float CrossHairYLocation = 0.33333f;
+	   
 protected:
 	// virtual significa que puede ser ampliado por sus herederos
 	// override significa que amplia la definicion de sus ancestros
